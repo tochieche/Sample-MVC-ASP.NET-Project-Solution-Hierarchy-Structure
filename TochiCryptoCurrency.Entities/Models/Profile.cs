dd4941eec7660b0db935dc83b66adc787b1238bd
Tochukwu Okafor <tochieche@gmail.com>
2018-08-28 17:35:16 +0100

    Final Project Structure

---
+++ TochiCryptoCurrency.Entities/Models/Profile.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TochiCryptoCurrency.Entities.Models
+{
+    class Profile
+    {
+    }
+}
---
+++ TochiCryptoCurrency.Entities/ViewModels/UsersVM/RegisterViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TochiCryptoCurrency.Entities.ViewModels.UsersVM
+{
+    class RegisterViewModel
+    {
+    }
+}
