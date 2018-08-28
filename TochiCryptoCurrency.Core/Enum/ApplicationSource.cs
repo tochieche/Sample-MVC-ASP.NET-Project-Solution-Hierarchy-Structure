8f9b842889bc72e87d8852d96d066de9ec78d0aa
Tochukwu Okafor <tochieche@gmail.com>
2018-08-28 17:44:22 +0100

    Final Update

---
+++ TochiCryptoCurrency.Core/Enum/ApplicationSource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TochiCryptoCurrency.Core.Enum
+{
+    public enum ApplicationSource
+    {
+        Web = 1,
+        Destop = 2,
+        Mobile = 3,
+        API = 4
+    }
+}
